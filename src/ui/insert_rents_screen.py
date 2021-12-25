55b13a63e4ffffda1db0c03c4dad33ae66a81a15
Miika Rannikko <miika.rannikko@gmail.com>
2021-12-25 16:18:43 +0200

    Input validators to rents

--- src/ui/insert_rents_screen.py
+++ src/ui/insert_rents_screen.py
@@ -30,18 +30,55 @@ class InsertRentsScreen:
     def _main_menu(self):
         self._go_to_menu_screen()
 
+    def _input_not_empty_check(self, input_to_check, label=""):
+        if input_to_check == "":
+            messagebox.showinfo(title='Invalid input', message=f'{label} must not be empty', icon='error')
+            return False
+        else:
+            return True
+
+    def _input_integer_check(self, input_to_check, label=""):
+        try:
+            int(input_to_check)
+            return True
+        except ValueError:
+            messagebox.showinfo(title='Invalid input', message=f'{label} must be an integer', icon='error')
+            return False
+
+    def _input_float_check(self, input_to_check, label=""):
+        try:
+            float(input_to_check)
+            return True
+        except ValueError:
+            messagebox.showinfo(title='Invalid input', message=f'{label} must be an number', icon='error')
+            return False
+
     def create_rent_to_database(self):
         chosen_unit_id = self._unit_id_field_input.get()
         chosen_start_date = self._start_date_field_input.get_date()
         chosen_amount = self._amount_field_input.get()
         chosen_due_dom = self._due_dom_field_input.get()
-        if self._end_previous_rent_field_state.instate(['selected']):
+
+        if self._input_not_empty_check(chosen_unit_id, label='Chosen unit id') ==  False:
+            self._stay_on_screen()
+        elif self._input_integer_check(chosen_unit_id, label='Chosen unit id') == False:
+            self._stay_on_screen()
+        elif self._input_not_empty_check(chosen_start_date, label='Chosen start date') ==  False:
+            self._stay_on_screen()
+        elif self._input_not_empty_check(chosen_amount, label='Chosen amount') ==  False:
+            self._stay_on_screen()
+        elif self._input_float_check(chosen_amount, label='Chosen amount') ==  False:
+            self._stay_on_screen()
+        elif self._end_previous_rent_field_state.instate(['selected']):
             rent_id_to_end = landlord_service.get_latest_rent_id(chosen_unit_id)
             landlord_service.end_rent(rent_id_to_end, chosen_start_date)
             landlord_service.create_rent(chosen_unit_id, chosen_start_date, chosen_amount, chosen_due_dom)
+            messagebox.showinfo(title='Success', message=f'Lease succesfully created')
+            self._main_menu()
         else:
-            landlord_service.create_maintenance_charge(chosen_unit_id, chosen_start_date, chosen_amount, chosen_due_dom)
-        self._main_menu()
+            landlord_service.create_rent(chosen_unit_id, chosen_start_date, chosen_amount, chosen_due_dom)
+            messagebox.showinfo(title='Success', message=f'Lease succesfully created')
+            self._main_menu()
 
     def initialize(self):
         self._frame = Frame(master=self._root)
@@ -54,7 +91,7 @@ class InsertRentsScreen:
             year=this_year, day=1, month=1, date_pattern='YYYY/MM/DD')  
         amount_label = Label(master=self._frame, text='Amount')
         self._amount_field_input = Entry(master=self._frame)  
-        due_dom_label = Label(master=self._frame, text='Rent due day of month')
+        due_dom_label = Label(master=self._frame, text='Rent due day of month (optional)')
         self._due_dom_field_input = Entry(master=self._frame) 
         end_previous_label = Checkbutton(master=self._frame, text='End previous rent') 
         self._end_previous_rent_field_state = end_previous_label
